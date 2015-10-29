a62480f26dac6fe07516fee42a7c95d2b68ff594
Nikolay Kostov <admin@nikolay.it>
2015-10-29 19:13:07 +0200

    Added empty project TexasHoldem.AI.SmartPlayer

---
+++ Source/AI/TexasHoldem.AI.SmartPlayer/SmartPlayer.cs
@@ -0,0 +1,15 @@
+﻿namespace TexasHoldem.AI.SmartPlayer
+{
+    using TexasHoldem.Logic.Players;
+
+    public class SmartPlayer : BasePlayer
+    {
+        public override string Name => "Smart player";
+
+        public override PlayerAction GetTurn(GetTurnContext context)
+        {
+            // TODO: Implement smart logic
+            return PlayerAction.Fold();
+        }
+    }
+}

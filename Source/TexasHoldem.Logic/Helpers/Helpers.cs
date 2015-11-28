f0426c16a353ef52f4c3e32ed09c4c58de5fe95b
damy90 <dany90@gmail.com>
2015-11-28 18:57:07 +0200

    Added documentation to TexasHoldem.Logic\Helpers Helpers.cs

--- Source/TexasHoldem.Logic/Helpers/Helpers.cs
+++ Source/TexasHoldem.Logic/Helpers/Helpers.cs
@@ -4,15 +4,31 @@
 
     using TexasHoldem.Logic.Cards;
 
+    /// <summary>
+    /// Class containing helper methods for evaluating and comparing player's hands.
+    /// </summary>
     public static class Helpers
     {
         private static readonly IHandEvaluator HandEvaluator = new HandEvaluator();
 
+
+        /// <summary>
+        /// Finds the best possible hand given a player's cards and all revealed comunity cards.
+        /// </summary>
+        /// <param name="cards">A player's cards + all revealed comunity cards</param>
+        /// <returns>Returns value of HandRankType. For example Straight, Flush, etc</returns>
         public static HandRankType GetHandRank(ICollection<Card> cards)
         {
             return HandEvaluator.GetBestHand(cards).RankType;
         }
 
+        /// <summary>
+        /// Compares the cards of two opponents to see which one can make the strongest hand with the community cards.
+        /// At least 5 cards are needed. Can be used during and after the Flop round.
+        /// </summary>
+        /// <param name="firstPlayerCards">First player cards + all revealed comunity cards</param>
+        /// <param name="secondPlayerCards">Second player cards + all revealed comunity cards</param>
+        /// <returns>Comparison result as int</returns>
         public static int CompareCards(IEnumerable<Card> firstPlayerCards, IEnumerable<Card> secondPlayerCards)
         {
             var firstPlayerBestHand = HandEvaluator.GetBestHand(firstPlayerCards);

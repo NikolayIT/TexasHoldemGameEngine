51dc0a8bb613889a8f7789f83c7b43f40628522e
Nikolay Kostov <admin@nikolay.it>
2015-12-01 12:20:50 +0200

    Added AlwaysRaiseDummyPlayer

---
+++ Source/AI/TexasHoldem.AI.DummyPlayer/AlwaysRaiseDummyPlayer.cs
@@ -0,0 +1,16 @@
+﻿namespace TexasHoldem.AI.DummyPlayer
+{
+    using System;
+
+    using TexasHoldem.Logic.Players;
+
+    internal class AlwaysRaiseDummyPlayer : BasePlayer
+    {
+        public override string Name { get; } = "AlwaysRaiseDummyPlayer_" + Guid.NewGuid();
+
+        public override PlayerAction GetTurn(GetTurnContext context)
+        {
+            return PlayerAction.Raise(context.SmallBlind);
+        }
+    }
+}

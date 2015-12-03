276f794c1674d336d0e4d7b22a8f37786684378f
Nikolay Kostov <admin@nikolay.it>
2015-12-03 22:22:18 +0200

    Added AlwaysAllInDummyPlayer

---
+++ Source/AI/TexasHoldem.AI.DummyPlayer/AlwaysAllInDummyPlayer.cs
@@ -0,0 +1,23 @@
+﻿namespace TexasHoldem.AI.DummyPlayer
+{
+    using System;
+
+    using TexasHoldem.Logic.Players;
+
+    internal class AlwaysAllInDummyPlayer : BasePlayer
+    {
+        public override string Name { get; } = "AlwaysAllInDummyPlayer_" + Guid.NewGuid();
+
+        public override PlayerAction GetTurn(GetTurnContext context)
+        {
+            if (context.MoneyLeft > 0)
+            {
+                return PlayerAction.Raise(context.MoneyLeft);
+            }
+            else
+            {
+                return PlayerAction.CheckOrCall();
+            }
+        }
+    }
+}

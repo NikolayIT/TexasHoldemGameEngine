d72c3b10ed286f7e910d0bea4af5b8e5e60a4e45
dalorveen <dalorveen@gmail.com>
2018-01-15 23:29:00 +0500

    The logic of the method "DetermineWinnerAndAddPot" now works for multiple players.

--- Source/TexasHoldem.Logic/GameMechanics/MultiplePlayersHandLogic.cs
+++ Source/TexasHoldem.Logic/GameMechanics/MultiplePlayersHandLogic.cs
@@ -16,7 +16,70 @@
 
         protected override void DetermineWinnerAndAddPot(int pot)
         {
-            throw new System.NotImplementedException();
+            if (this.Players.Count(x => x.PlayerMoney.InHand) == 1)
+            {
+                var winner = this.Players.FirstOrDefault(x => x.PlayerMoney.InHand);
+                winner.PlayerMoney.Money += pot;
+            }
+            else
+            {
+                // showdown
+                foreach (var player in this.Players)
+                {
+                    if (player.PlayerMoney.InHand)
+                    {
+                        this.ShowdownCards.Add(player.Name, player.Cards);
+                    }
+                }
+
+                // Bubble sort
+                var players = this.Players.ToArray();
+                for (int element = 0; element < players.Length - 1; element++)
+                {
+                    for (int i = 0; i < players.Length - (element + 1); i++)
+                    {
+                        if (Helpers.CompareCards(
+                            players[i].Cards.Concat(this.CommunityCards),
+                            players[i + 1].Cards.Concat(this.CommunityCards)) < 0)
+                        {
+                            var temp = players[i + 1];
+                            players[i + 1] = players[i];
+                            players[i] = temp;
+                        }
+                    }
+                }
+
+                int next = 1;
+                do
+                {
+                    var betterHand = Helpers.CompareCards(
+                        players[0].Cards.Concat(this.CommunityCards),
+                        players[next].Cards.Concat(this.CommunityCards));
+                    if (betterHand > 0)
+                    {
+                        if (next == 1)
+                        {
+                            players[0].PlayerMoney.Money += pot;
+                            break;
+                        }
+                    }
+                    else if (betterHand == 0)
+                    {
+                        next++;
+                        if (next <= players.Count() - 1)
+                        {
+                            continue;
+                        }
+                    }
+
+                    for (int i = 0; i < next; i++)
+                    {
+                        // Divide the pot among two or more players
+                        players[i].PlayerMoney.Money += pot / next;
+                    }
+                }
+                while (true);
+            }
         }
     }
 }

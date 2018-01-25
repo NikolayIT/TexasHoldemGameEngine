2cadd39a26ecf00d79c492625b0694d85ee32101
dalorveen <dalorveen@gmail.com>
2018-01-25 14:17:45 +0500

    feat: constructor for calculator

--- Source/AI/TexasHoldem.AI.SelfLearningPlayer/PokerMath/Calculator.cs
+++ Source/AI/TexasHoldem.AI.SelfLearningPlayer/PokerMath/Calculator.cs
@@ -7,10 +7,47 @@
 
     public class Calculator : ICalculator
     {
+        private readonly ulong hero;
+
+        private readonly IList<ulong> opponents;
+
+        private readonly ulong communityCards;
+
         public Calculator(
-            ICollection<Card> heroHoleCards, ISet<ICollection<Card>> opponentsHoleCards, ICollection<Card> communityCards)
+            ICollection<Card> heroHoleCards, IList<ICollection<Card>> opponentsHoleCards, ICollection<Card> communityCards)
         {
-            throw new NotImplementedException();
+            if (heroHoleCards == null)
+            {
+                throw new ArgumentNullException(nameof(heroHoleCards));
+            }
+            else if (heroHoleCards.Count != 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(heroHoleCards), "At least two hole cards are required");
+            }
+
+            if (opponentsHoleCards == null)
+            {
+                throw new ArgumentNullException(nameof(opponentsHoleCards));
+            }
+            else if (opponentsHoleCards.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(opponentsHoleCards), "At least two hole cards are required");
+            }
+
+            if (communityCards == null)
+            {
+                throw new ArgumentNullException(nameof(communityCards));
+            }
+
+            this.hero = new CardAdapter(heroHoleCards).Mask;
+
+            this.opponents = new List<ulong>();
+            foreach (var item in opponentsHoleCards)
+            {
+                this.opponents.Add(new CardAdapter(item).Mask);
+            }
+
+            this.communityCards = new CardAdapter(communityCards).Mask;
         }
 
         public double Equity()
